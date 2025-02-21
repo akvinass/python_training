0be52be8fe929569e8a59b45b7958278bde250d2
akvinass <akvinass@gmail.com>
2025-02-21 18:13:34 +0200

    updated with additional tasks from Lecture4

--- Lecture 4/lecture4_addon3.py
+++ Lecture 4/lecture4_addon3.py
@@ -1,8 +1,10 @@
 import random
 x = random.randint (1, 100)
 #print(x)
-user_input = int(input('input a positive number: '))
-
+user_input = int(input('input a positive number from 1 to 100: '))
+if user_input not in range(1, 100):
+    print('The number should be within a range of 1-100!')
+    user_input = int(input('input a positive number from 1 to 100: '))
 while user_input != x:
     if user_input < x:
         print('Warmer!')
