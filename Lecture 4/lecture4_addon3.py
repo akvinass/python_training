6f16051f2a5dbc876185eea5126a1f655950e6aa
akvinass <akvinass@gmail.com>
2025-02-21 18:27:58 +0200

    added an input number verifier to addon 3

--- Lecture 4/lecture4_addon3.py
+++ Lecture 4/lecture4_addon3.py
@@ -1,15 +1,14 @@
 import random
 x = random.randint (1, 100)
 #print(x)
-user_input = int(input('input a positive number from 1 to 100: '))
-if user_input not in range(1, 100):
-    print('The number should be within a range of 1-100!')
-    user_input = int(input('input a positive number from 1 to 100: '))
-while user_input != x:
-    if user_input < x:
-        print('Warmer!')
-    else:
-        print('Colder!')
-    user_input = int(input('input a positive number: '))
+user_input = int(input('Input a positive number from 1 to 100: '))
 
+while user_input != x:
+    if user_input not in range(1, 100):
+        print('The number is out of range!')
+    elif user_input < x:
+        print('Warmer!')
+    elif user_input > x:
+        print('Colder!')
+    user_input = int(input('Input a positive number from 1 to 100: '))
 print('That is correct!')
