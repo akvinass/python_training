69bb78fca7952fe6f14c7fb7a7650c4729dc1dc2
akvinass <akvinass@gmail.com>
2025-02-25 00:14:06 +0200

    added addon 3 (Caesar cypher) to lecture 5

---
+++ Lecture 5/lecture5_addon3.py
@@ -0,0 +1,13 @@
+import string
+
+user_input = input('Input: ')
+letters = ' '.join(string.ascii_letters).split()
+output = []
+
+for i in user_input:
+    if i in letters:
+        x =  letters.index(i) + 3
+        output.append(letters[x])
+    else:
+        output.append(i)
+print('Encoded value:', ''.join(output))
